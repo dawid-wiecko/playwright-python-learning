53eccaa683607328cfe0816b4924513ce0b49697
dawid.wiecko <dawid.wiecko@wp.pl>
2026-05-12 22:09:25 +0200

    Refactoring: dismiss_cookies_popup when cookies popup appeared

--- Pages/AutomationExercise/HomePage.py
+++ Pages/AutomationExercise/HomePage.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright, Page, expect
 
 def test_dismiss_cookies_popup():
     with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False, args=["--disable-cookies"])
+        browser = playwright.chromium.launch(headless=False, args=["--disable-cookies"],)
         page = browser.new_page()
         page.goto("https://automationexercise.com/")
         dismiss_cookies_popup(page)
@@ -11,7 +11,9 @@ def test_dismiss_cookies_popup():
 
 def dismiss_cookies_popup(page: Page):
     button_manage_options = page.get_by_role("button", name="Zarządzaj opcjami")
-    expect(button_manage_options).to_be_visible()
+    is_visible = button_manage_options.is_visible()
+    if not is_visible:
+        return
     button_manage_options.click()
 
     button_confirm_selected_options = page.get_by_role("button", name="Potwierdź wybrane opcje")
